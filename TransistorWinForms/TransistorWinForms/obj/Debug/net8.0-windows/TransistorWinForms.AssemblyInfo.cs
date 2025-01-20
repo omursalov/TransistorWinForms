fa09bf36d183e5dddf33e9b8b8aa7dfc46f007b8
omursalov <oleg.mursalove@gmail.com>
2025-01-20 18:12:22 +0300

    controls

--- TransistorWinForms/TransistorWinForms/obj/Debug/net8.0-windows/TransistorWinForms.AssemblyInfo.cs
+++ TransistorWinForms/TransistorWinForms/obj/Debug/net8.0-windows/TransistorWinForms.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TransistorWinForms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2acb3d2296e1338d6659a7b3263a915946485bb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("TransistorWinForms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TransistorWinForms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
